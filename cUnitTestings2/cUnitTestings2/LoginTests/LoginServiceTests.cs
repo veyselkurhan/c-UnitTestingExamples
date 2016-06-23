358586d3772d552d2ee5c7fe1b06a2deccae99b4
veyselkurhan <veyselkurhan16@gmail.com>
2016-06-23 15:54:29 +0300

    tekrar ekle

---
+++ cUnitTestings2/cUnitTestings2/LoginTests/LoginServiceTests.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using FakeItEasy;
+namespace cUnitTestings2.LoginTests
+{
+    [TestFixture]
+    public class LoginServiceTests
+    {
+        private IAccountRepository accountRepository;
+        private IAccount account;
+        private LoginService service;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.account = A.Fake<IAccount>();
+            this.accountRepository = A.Fake<IAccountRepository>();
+            A.CallTo(() => accountRepository.Find(A<string>.Ignored)).Returns(this.account);
+            this.service = new LoginService(accountRepository);
+
+        }
+
+        [Test]
+        public void SecondLogin_AlreadyLogin_NotPermitAgainLogin()
+        {
+            //Arrange
+            A.CallTo(() => this. account.PasswordMatches(A<string>.Ignored)).Returns(true);
+
+            //act
+            service.Login("username","password");
+            service.Login("username","password");
+            //Assert
+            A.CallTo(()=>this.account.SetLoggedIn(true)).MustHaveHappened(Repeated.Exactly.Once);
+
+        }
+
+        [Test]
+        public void NotRevokeTest_twofailLoginAttempts_NotRevokeUser()
+        {
+            //Arrange
+            A.CallTo(() => this.account.PasswordMatches(A<string>.Ignored)).Returns(false);
+            
+            //act
+            this.service.Login("username","wrong password");
+            this.service.Login("username","wrong password");
+            this.service.Login("vk","wrong password");
+
+            A.CallTo(()=>this.account.SetRevoked(true)).MustNotHaveHappened();
+        }
+        [Test]
+        public void LoginTest_paswordMatches_loggedIn()
+        {
+            //Arrange
+            A.CallTo(() =>this. account.PasswordMatches(A<string>.Ignored)).Returns(true);
+            //Act
+           this. service.Login("username", "password");
+            //Assert
+            A.CallTo(() =>this. account.SetLoggedIn(true)).MustHaveHappened();
+
+        }
+
+        [Test]
+        public void RevokeAccount_3FailLoginAttempts_RevokeAccount()
+        {
+            //Arrange
+            A.CallTo(() =>this. account.PasswordMatches(A<string>.Ignored)).Returns(false);
+            //act
+           this. service.Login("username", "wrong password");
+            this.service.Login("username", "wrong password");
+            this.service.Login("username", "wrong password");
+            //Assert
+            A.CallTo(() => this.account.SetRevoked(true)).MustHaveHappened(Repeated.Exactly.Once);
+        }
+
+        [Test]
+        public void FailLogin_passwordWrong_NotLoggedIn()
+        {
+            //Arrange
+            A.CallTo(() =>this. account.PasswordMatches(A<string>.Ignored)).Returns(false);
+            //act
+            this.service.Login("username", "wrong password");
+            A.CallTo(() => this.account.SetLoggedIn(true)).MustNotHaveHappened();
+        }
+    }
+
+    public interface IAccount
+    {
+        
+        bool PasswordMatches(string password);
+        void SetLoggedIn(bool b);
+        void SetRevoked(bool b);
+
+       
+    }
+
+    public interface IAccountRepository
+    {
+        IAccount Find(string name);
+    }
+
+    public class LoginService
+    {
+        private int loginCount;
+        private int numberOfFailedAttempts = 0;
+        private IAccountRepository accountRepository;
+        private string previousLoginUsername;
+        public LoginService(IAccountRepository accountRepository)
+        {
+            this. loginCount = 0;
+            this.accountRepository = accountRepository;
+            this.previousLoginUsername=string.Empty;
+        }
+        public void Login(string username, string password)
+        {
+
+            var account = this.accountRepository.Find(username);
+
+         if (account.PasswordMatches(password)&&loginCount==0)
+         {
+             loginCount = 1;
+            account.SetLoggedIn(true);
+            }
+
+            if (!account.PasswordMatches(password) && this.previousLoginUsername.Equals(username))
+            {
+                this.numberOfFailedAttempts++;
+            }
+            else
+            {
+                this.numberOfFailedAttempts = 1;
+                this.previousLoginUsername = username;
+            }
+            if (this.numberOfFailedAttempts == 3)
+            {
+                account.SetRevoked(true);
+            }
+        }
+    }
+}
---
+++ cUnitTestings2/cUnitTestings2/TakeItEasy/TestingFindingForAprofessinal.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using FakeItEasy;
+using NUnit.Framework;
+
+namespace cUnitTestings2.NewFolder1
+{
+    [TestFixture]
+    internal class TestingFindingForAprofessinal
+    {
+        [Test]
+        public void UserRegistration_CorrectInformtaion_ReturnsTrue()
+        {
+            var id = 1;
+            var name = "veysel";
+            var mail = "vysl1com";
+            var password = "123456";
+            var type = "worker";
+            Worker worker = null;
+            var users = A.Fake<IUsers>();
+            A.CallTo(() => users.controlInformation(name, mail, password, type))
+                .Returns(worker = new Worker(name, mail, password, type));
+            var database = A.Fake<IUsersInformation>();
+            A.CallTo(() => database.Insert(worker)).Returns(worker);
+            var registration = new RegistrationService();
+            var returnValue = registration.Insert(database, worker);
+            Assert.True(returnValue);
+            {
+            }
+        }
+    }
+
+    public interface IUsers
+    {
+        IUsers controlInformation(string name, string mail, string password, string type);
+    }
+
+    public class Worker : IUsers
+    {
+        public Worker(string name, string mail, string password, string type)
+        {
+        }
+
+
+        public IUsers controlInformation(string name, string mail, string password, string type)
+        {
+            Console.Write("deneme");
+            if (password.Length > 5 && mail.EndsWith("@%.com"))
+            {
+                return new Customer(name, mail, password, type);
+            }
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Customer : IUsers
+    {
+        public Customer(string name, string mail, string password, string type)
+        {
+        }
+
+        public IUsers controlInformation(string name, string mail, string password, string type)
+        {
+            if (password.Length > 5 && mail.EndsWith("@%.com"))
+            {
+                return new Customer(name, mail, password, type);
+            }
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IUsersInformation
+    {
+        IUsers Insert(IUsers u);
+        bool Contains(IUsers u);
+    }
+
+    public class RegistrationService
+    {
+        public bool Insert(IUsersInformation database, IUsers u)
+        {
+          
+            if (!database.Contains(u)) return true;
+            return true;
+        }
+    }
+}
